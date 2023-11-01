ca64ad2d94c61230f3805a33e136fb377760eb59
Cendrine Briard <cendrine_briard@protonmail.com>
2023-11-01 11:11:44 +0100

    css fix, img optim

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,12 +3,12 @@ import PropTypes from "prop-types"
 import './Button.css'
 
 
-  const Button = ({  onClick,  disabled, children }) => {
+  const Button = ({  onClick,  disabled,className, children }) => {
    
         return (
           <button          
             disabled={disabled}
-            className="button"
+            className={className}
             onClick={onClick}
           >
             {children}
@@ -18,11 +18,13 @@ import './Button.css'
   Button.propTypes = {
     onClick: PropTypes.func,
     disabled: PropTypes.bool,
+    className:PropTypes.string,
     children: PropTypes.node,
   };
   Button.defaultProps = {
     disabled: false,
     onClick: () => null,
+    className: "button",
     children: null
   }
   
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,9 +3,11 @@ import './Header.css'
 import {Link} from 'react-router-dom'
 import BankLogo from'../../assets/argentBankLogo.png'
 import UserLogo from '../../assets/circle-user-solid.svg'
+import signOut from'../../assets/sign-out.svg'
 import { useDispatch, useSelector } from 'react-redux'
 import { logout } from '../../slice/AuthSlice'
 import { logoutUser } from '../../slice/UserSlice'
+import Button from '../Button/Button'
 
 
 
@@ -33,20 +35,17 @@ const loggingout=()=>{
 
         {user?(<div className='main-nav-item'  >
             <Link to= '/profile'>
-            <div>
-                < img src ={UserLogo} alt='icone de profil'/>
-                <span>{username}</span>
-                
+            <div className='main-nav-item'>
+                < img src ={UserLogo} alt='profile icon'/>
+                <span>{username}</span>                
             </div>
         </Link>
         <Link to= '/login'>
-            <div>
-                <button onClick={loggingout}>Sign out</button>
-            </div>
+            <Button className="main-nav-item" onClick={loggingout}>< img src ={signOut} alt='log out icon'/> Sign out</Button>
         </Link>
         </div>):(<><Link to= '/login'>
             <div className="main-nav-item" >
-                < img src ={UserLogo} alt='icone de profil'/>
+                < img src ={UserLogo} alt='profile icon'/>
                 <p>Sign In</p>
             </div>
         </Link></>)}
